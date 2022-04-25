a6f0c2d77f99eea551a73a98310dfe004bbb7b2f
stickyfingers489 <mutuadennis56@gmai.com>
2022-04-25 13:53:16 +0300

    This commit daels with the lazyvgrid. It also deals with the mssing App Icon.

--- InstagramApp/Components/Grid.swift
+++ InstagramApp/Components/Grid.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct Grid: View {
+    var images: [String] = ["profile", "profile2", "profile3", "profile4", "profile5", "profile6", "profile7", "profile8"]
+    
+    var columnGrid: [GridItem] = [GridItem(.flexible(), spacing: 1), GridItem(.flexible(), spacing: 1), GridItem(.flexible(), spacing: 1)]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LazyVGrid(columns: columnGrid, spacing: 1) {
+            ForEach((0...44), id: \.self) {
+                Image(images[$0 % images.count])
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: (UIScreen.main.bounds.width / 3) - 1, height: (UIScreen.main.bounds.width / 3) - 1)
+                    .clipped()
+            }
+        }
     }
 }
 
--- InstagramApp/Components/Stories.swift
+++ InstagramApp/Components/Stories.swift
@@ -18,7 +18,7 @@ struct Stories: View {
                 Story(image: "profile5", name: "Sam")
                 Story(image: "profile6", name: "Dara")
                 Story(image: "profile7", name: "Sourany")
-                Story(image: "profile9", name: "Abby")
+                Story(image: "profile9", name: "Abbie")
             }
             .padding(.horizontal, 8)
         }
